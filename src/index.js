dbe1c6d231182821d1fb276d2d975ed72fe526eb
eliforeman <ejfforeman@yahoo.com>
2018-12-22 03:00:56 -0500

    assets push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import iro from '@jaames/iro';
-
-
+var demoColorPicker = new iro.ColorPicker("#colorPickerContainer", {
+    // Set the size of the color picker UI
+    width: 320,
+    height: 320,
+    // Set the initial color to red
+    color: "#f00"
+  });
