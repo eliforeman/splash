9a7a1eac8f8bf94f4fe778966fa7a1932d3a21c3
Eli <ejfforeman@yahoo.com>
2019-07-15 20:00:08 -0400

    updating this app

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Using Basic Color Theory to Suggest an Outfit. 
 
 based on this article: https://lifehacker.com/learn-the-basics-of-color-theory-to-know-what-looks-goo-1608972072
+
+
+
+version 1:
+
--- src/index.js
+++
@@ -1,7 +0,0 @@
-var demoColorPicker = new iro.ColorPicker("#colorPickerContainer", {
-    // Set the size of the color picker UI
-    width: 320,
-    height: 320,
-    // Set the initial color to red
-    color: "#f00"
-  });
